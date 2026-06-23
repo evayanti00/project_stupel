af82e3a605080d6dc9731f8e2e34b2b581792d3f
evayanti00 <evayantimade9@gmail.com>
2026-06-23 22:23:44 +0800

    membuat form register

---
+++ lib/screens/auth/register_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import '../home/dashboard/dashboard_screen.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void _register() {
+    // sementara dummy register
+    if (nameController.text.isNotEmpty &&
+        emailController.text.isNotEmpty &&
+        passwordController.text.isNotEmpty) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const DashboardScreen()),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Lengkapi semua field")),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Register")),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(controller: nameController, decoration: const InputDecoration(labelText: "Nama")),
+            TextField(controller: emailController, decoration: const InputDecoration(labelText: "Email")),
+            TextField(controller: passwordController, decoration: const InputDecoration(labelText: "Password"), obscureText: true),
+            const SizedBox(height: 20),
+            ElevatedButton(onPressed: _register, child: const Text("Register")),
+          ],
+        ),
+      ),
+    );
+  }
+}

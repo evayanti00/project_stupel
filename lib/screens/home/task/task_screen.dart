38514864c0e1e1187933bcf2a7818105b14d8085
evayanti00 <evayantimade9@gmail.com>
2026-06-23 22:24:41 +0800

    menambahkan hlm

---
+++ lib/screens/home/task/task_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TaskScreen extends StatelessWidget {
+  const TaskScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Halaman Tugas"),
+    );
+  }
+}
+
